916c80a3065d8ab578680c04e13881ac7c24e84f
Krzysztof Kowalski <Kwrzos08@gmail.com>
2024-12-18 22:16:44 -0500

    ores

--- src/main/java/net/gamer/ancientshinobi/item/ModItems.java
+++ src/main/java/net/gamer/ancientshinobi/item/ModItems.java
@@ -3,7 +3,6 @@ package net.gamer.ancientshinobi.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.gamer.ancientshinobi.AncientShinobi;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -18,7 +17,10 @@ public class ModItems {
     public static final Item CUTTOPAZ = registerItem("cut_topaz", new Item(new Item.Settings()));
     public static final Item RAWPLATINUM = registerItem("raw_platinum", new Item(new Item.Settings()));
     public static final Item PLATINUMINGOT = registerItem("platinum_ingot", new Item(new Item.Settings()));
-
+    public static final Item RAWJADE = registerItem("raw_jade", new Item(new Item.Settings()));
+    public static final Item CUTJADE = registerItem("cut_jade", new Item(new Item.Settings()));
+    public static final Item RAWAZURE = registerItem("raw_azure", new Item(new Item.Settings()));
+    public static final Item CUTAZURE = registerItem("cut_azure", new Item(new Item.Settings()));
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(AncientShinobi.MOD_ID,name),item);
     }
@@ -36,6 +38,10 @@ public class ModItems {
             entries.add(CUTRUBY);
             entries.add(RAWPLATINUM);
             entries.add(PLATINUMINGOT);
+            entries.add(RAWJADE);
+            entries.add(CUTJADE);
+            entries.add(RAWAZURE);
+            entries.add(CUTAZURE);
         });
     }
 }
